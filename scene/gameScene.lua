6428b36cf5369db9bb2d5cee1ed8a4dbe0539fa2
wendiyu <36240636+wendiyu@users.noreply.github.com>
2018-05-28 20:36:47 -0400

    update code

--- scene/gameScene.lua
+++ scene/gameScene.lua
@@ -19,8 +19,11 @@ local scene = composer.newScene()
 -- you need these to exist the entire scene
 -- this is called "forward reference"
 local map = nil
-local knight = nil
+local ninjaBoy = nil
 local rightArrow = nil
+local jumpButton = nil
+local shootButton = nil
+local playerKunais ={}
  
 -- -----------------------------------------------------------------------------------
 -- Code outside of the scene event functions below will only be executed ONCE unless
@@ -29,34 +32,121 @@ local rightArrow = nil
 
 local function onRightArrowTouch( event )
     if (event.phase == "began") then
-        if (knight.sequence ~= "run") then
-            knight.sequence = "run"
-            knight:setSequence("run")
-            knight:play()
+        if (ninjaBoy.sequence ~= "run") then
+            ninjaBoy.sequence = "run"
+            ninjaBoy:setSequence("run")
+            ninjaBoy:play()
         end
 
     elseif (event.phase == "ended") then
-        if (knight.sequence ~= "idle") then
-            knight.sequence = "idle"
-            knight:setSequence("idle")
-            knight:play()
+        if (ninjaBoy.sequence ~= "idle") then
+            ninjaBoy.sequence = "idle"
+            ninjaBoy:setSequence("idle")
+            ninjaBoy:play()
         end
     end
     return true
     
 end
 
-local moveKnight = function( event )
+local function onJumpButtonTouch( event )
+    if (event.phase == "began") then
+        if (ninjaBoy.sequence ~= "jump") then
+            ninjaBoy.sequence = "jump"
+            ninjaBoy:setSequence("jump")
+            ninjaBoy:play()
+        end
+
+    elseif (event.phase == "ended") then
+        if (ninjaBoy.sequence ~= "idle") then
+            ninjaBoy.sequence = "idle"
+            ninjaBoy:setSequence("idle")
+            ninjaBoy:play()
+        end
+    end
+    return true
     
-    if knight.sequence == "run" then
-        transition.moveBy( knight, {
+end
+
+local moveNinjaBoy = function( event )
+    
+    if ninjaBoy.sequence == "run" then
+        transition.moveBy( ninjaBoy, {
             x = 20,
             y = 0,
             time = 0
             })
     end
-end
  
+    if ninjaBoy.sequence == "jump" then
+       -- can also check if the ninjaBoy has landed from a jump
+       local ninjaBoyVelocityX, ninjaBoyVelocityY = ninjaBoy:getLinearVelocity()
+  
+       if ninjaBoyVelocityY == 0 then
+          -- the ninjaBoy is currently not jumping
+          -- it was jumping so set to idle
+          ninjaBoy.sequence = "idle"
+          ninjaBoy:setSequence( "idle" )
+          ninjaBoy:play()
+       end
+  
+    end  
+end
+
+local ninjaBoyThrow = function( event )
+     -- after 1 second go back to idle
+     ninjaBoy.sequence = "idle"
+     ninjaBoy:setSequence( "idle" )
+     ninjaBoy:play()
+ end
+
+
+local checkPlayerKunaisOutOfBounds = function ( event )
+	-- check if any bullets have gone off the screen
+	local kunaisCounter
+
+    if #playerKunais > 0 then
+        for kunaisCounter = #playerKunais, 1 ,-1 do
+            if playerKunais[kunaisCounter].x > display.contentWidth *2 then
+                playerKunais[kunaisCounter]:removeSelf()
+                playerKunais[kunaisCounter] = nil
+                table.remove(playerKunais, kunaisCounter)
+                print("remove kunais")
+            end
+        end
+    end
+end
+
+local function onshootButtonTouch( event )
+    if ( event.phase == "began" ) then
+        if (ninjaBoy.sequence ~= "throw") then
+            ninjaBoy.sequence = "throw"
+            ninjaBoy:setSequence("throw")
+            ninjaBoy:play()
+            timer.performWithDelay( 1000, ninjaBoyThrow )
+   
+        -- make a bullet appear
+        local aSingleKunai = display.newImage( "./assets/sprites/Kunai.png" )
+        aSingleKunai.x = knight.x
+        aSingleKunai.y = knight.y 
+        physics.addBody( aSingleKunai, 'dynamic' )
+        -- Make the object a "bullet" type object
+        aSingleKunai.isBullet = true
+        aSingleKunai.isFixedRotation = true
+        aSingleKunai.gravityScale = 0
+        aSingleKunai.id = "bullet"
+        aSingleKunai:setLinearVelocity( 1500, 0 )
+
+        table.insert(playerKunais,aSingleKunai)
+        print("# of bullet: " .. tostring(#playerKunais))
+    end
+
+  elseif ( event.phase == "ended" ) then
+  
+  end
+    return true
+end
+
 -- -----------------------------------------------------------------------------------
 -- Scene event functions
 -- -----------------------------------------------------------------------------------
@@ -70,11 +160,17 @@ function scene:create( event )
     physics.setGravity(0, 32)
     --physics.setDrawMode("hybrid")
     -- get character
-    local sheetOptionsIdle = require("assets.spritesheets.knight.knightIdle")
-    local sheetIdleKnight = graphics.newImageSheet("./assets/spritesheets/knight/knightIdle.png", sheetOptionsIdle:getSheet() )
+    local sheetOptionsIdle = require("assets.spritesheets.ninjaBoy.ninjaBoyIdle")
+    local sheetIdleNinja = graphics.newImageSheet("./assets/spritesheets/ninjaBoy/ninjaBoyIdle.png", sheetOptionsIdle:getSheet() )
 
-    local sheetOptionsRun = require("assets.spritesheets.knight.knightRun")
-    local sheetRunningKnight = graphics.newImageSheet("./assets/spritesheets/knight/knightRun.png", sheetOptionsRun:getSheet() )
+    local sheetOptionsRun = require("assets.spritesheets.ninjaBoy.ninjaBoyRun")
+    local sheetRunningNinja = graphics.newImageSheet("./assets/spritesheets/ninjaBoy/ninjaBoyRun.png", sheetOptionsRun:getSheet() )
+ 
+    local sheetOptionsThrow = require("assets.spritesheets.ninjaBoy.ninjaBoyThrow")
+    local sheetThrowNinja = graphics.newImageSheet("./assets/spritesheets/ninjaBoy/ninjaBoyThrow.png", sheetOptionsThrow:getSheet() )
+ 
+    local sheetOptionsJump = require("assets.spritesheets.ninjaBoy.ninjaBoyJump")
+    local sheetJumpingNinja = graphics.newImageSheet("./assets/spritesheets/ninjaBoy/ninjaBoyJump.png", sheetOptionsJump:getSheet() )
 
     local sequence_data_ninja = {
     -- consecutive frames sequence
@@ -84,7 +180,7 @@ function scene:create( event )
         count = 10,
         time = 800,
         loopCount = 0,
-        sheet = sheetIdleKnight
+        sheet = sheetIdleNinja
     }, 
     {
         name = "run",
@@ -92,24 +188,40 @@ function scene:create( event )
         count = 10,
         time = 1000,
         loopCount = 0,
-        sheet = sheetRunningKnight
-    }             
+        sheet = sheetRunningNinja
+    },
+    {
+        name = "throw",
+        start = 1,
+        count = 10,
+        time = 1000,
+        loopCount = 1,
+        sheet = sheetThrowNinja
+    },
+    {
+        name = "jump",
+        start = 1,
+        count = 10,
+        time = 1000,
+        loopCount = 1,
+        sheet = sheetJumpingNinja
+    } 
 }
-        knight = display.newSprite( sheetIdleKnight, sequence_data_ninja )
-        knight.x = display.contentWidth * 0.5
-        knight.y = 0
-        knight.id = "knight"
-        knight.sequence = "idle"
+        ninjaBoy = display.newSprite( sheetIdleNinja, sequence_data_ninja )
+        ninjaBoy.x = display.contentWidth * 0.5
+        ninjaBoy.y = 0
+        ninjaBoy.id = "ninjaBoy"
+        ninjaBoy.sequence = "idle"
         -- add physics
-        physics.addBody(knight, "dynamic", {
+        physics.addBody(ninjaBoy, "dynamic", {
             density = 2.5,
             friction = 0.1,
             bounce = 0.2
             })
-        knight.isFixedRotation = true -- If you apply this property before the physics.addBody() command for the object, it will merely be treated as a property of the object like any other custom property and, in that case, it will not cause any physical change in terms of locking rotation. 
+        ninjaBoy.isFixedRotation = true -- If you apply this property before the physics.addBody() command for the object, it will merely be treated as a property of the object like any other custom property and, in that case, it will not cause any physical change in terms of locking rotation. 
 
-        knight:setSequence( "idle" )
-        knight:play()
+        ninjaBoy:setSequence( "idle" )
+        ninjaBoy:play()
 
         -- add right arrow
         rightArrow = display.newImage("./assets/sprites/rightButton.png")
@@ -117,14 +229,30 @@ function scene:create( event )
         rightArrow.y = display.contentHeight - 150
         rightArrow.alpha = 0.5
         rightArrow.id = "right arrow"
+ 
+        jumpButton = display.newImage("./assets/sprites/jumpButton.png")
+        jumpButton.x =  display.contentWidth - 80
+        jumpButton.y = display.contentHeight - 80
+        jumpButton.alpha = 0.5
+        jumpButton.id = "jump Button"
+ 
+        shootButton = display.newImage("./assets/sprites/shootButton.png")
+        shootButton.x =  display.contentWidth - 250
+        shootButton.y = display.contentHeight - 80
+        shootButton.alpha = 0.5
+        shootButton.id = "shoot Button"
+ 
+ 
 
         local filename = "assets/maps/level0.json" 
         local mapData = json.decodeFile( system.pathForFile( filename, system.ResourceDirectory ) )
         map = tiled.new( mapData, "assets/maps" )
 
         sceneGroup:insert( map )
-        sceneGroup:insert( knight )
+        sceneGroup:insert( ninjaBoy )
         sceneGroup:insert( rightArrow )
+        sceneGroup:insert( jumpButton )
+        sceneGroup:insert( shootButton )
 
 end
  
@@ -138,10 +266,13 @@ function scene:show( event )
     if ( phase == "will" ) then
         -- add in code to check character movement
         rightArrow:addEventListener( "touch", onRightArrowTouch )
+        jumpButton:addEventListener( "touch", onJumpButtonTouch )
+        shootButton:addEventListener( "touch", onshootButtonTouch )
  
     elseif ( phase == "did" ) then
         -- Code here runs when the scene is entirely on screen
-        Runtime:addEventListener ( "enterFrame", moveKnight )
+        Runtime:addEventListener ( "enterFrame", moveNinjaBoy )
+        Runtime:addEventListener ( "enterFrame", checkPlayerKunaisOutOfBounds )
  
     end
 end
@@ -155,10 +286,14 @@ function scene:hide( event )
  
     if ( phase == "will" ) then
         -- Code here runs when the scene is on screen (but is about to go off screen)
+        rightArrow:removeEventListener( "touch", onRightArrowTouch )
+        jumpButton:removeEventListener( "touch", onJumpButtonTouch )
+        shootButton:removeEventListener( "touch", onshootButtonTouch )
  
     elseif ( phase == "did" ) then
         -- Code here runs immediately after the scene goes entirely off screen
-        Runtime:removeEventListener ( "enterFrame", moveKnight )
+        Runtime:removeEventListener ( "enterFrame", moveNinjaBoy )
+        Runtime:removeEventListener ( "enterFrame", checkPlayerKunaisOutOfBounds )
  
     end
 end
